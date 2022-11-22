44eb14a651b5ebdd1a205e6ea57c9c4f1ea5318e
Mihail <viktor.svorikin@gmail.com>
2022-11-22 19:46:08 +0100

    Text for the welcome the users

---
+++ HelloTextes.py
@@ -0,0 +1,34 @@
+def print_hello_text_eng():
+    print("///__________________________________________________///")
+    print("///               Hello my dear user :)              ///")
+    print("///   You are welcomed by the 'UniConverse' program  ///")
+    print("///Here you can transfer currency at the current rate///")
+
+
+def print_hello_text_fr():
+    print("///_____________________________________________________///")
+    print("///           Bonjour mon cher utilisateur :)           ///")
+    print("/// Vous êtes accueilli par le programme 'UniConverse'  ///")
+    print("///ici vous pouvez transférer des devises au taux actuel///")
+
+
+def print_hello_text_sp():
+    print("///_________________________________________________///")
+    print("///        Hola mi querida usuaria/o :)             ///")
+    print("///  Le da la bienvenida el programa 'UniConverse'  ///")
+    print("///  aquí puede transferir moneda a la tasa actual  ///")
+
+
+def print_hello_text_de():
+    print("///_____________________________________________________///")
+    print("///              Hallo mein lieber User :)              ///")
+    print("///   Sie werden vom 'UniConverse' - Programm begrüßt,  ///")
+    print("///Hier können Sie Währung zum aktuellen Kurs überweisen///")
+
+
+def print_hello_text_srb():
+    print("///___________________________________________________///")
+    print("///           Здраво мој драги корисниче :)           ///")
+    print("///       Поздравља вас програм 'УниЦонверсе'         ///")
+    print("///   овде можете пренети валуту по тренутном курсу   ///")
+
